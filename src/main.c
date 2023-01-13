490f10a929da56b056ca653386a64b3b749429f4
schmaenjael <manuelmayer04@gmail.com>
2023-01-13 15:13:53 +0100

    feat(setup): add example setup for source and includes

---
+++ src/main.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include "include/pursuer.h"
+#include "tasks/pursuer.c"
+
+int main()
+{
+  printf("%d", add(5, 3));
+
+  return 0;
+}
---
+++ src/tasks/pursuer.c
@@ -0,0 +1,5 @@
+#include "include/pursuer.h"
+
+int add(int a, int b) {
+    return a + b;
+}
