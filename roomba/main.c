df9573b63c509987ee39c8599a8e4e50774f7a64
Michael Mayer <39913724+dwaltsch@users.noreply.github.com>
2023-02-28 15:50:56 +0100

    little mistake fixed

--- roomba/main.c
+++ roomba/main.c
@@ -1,68 +1,66 @@
 #include <stdbool.h>
 #include "kipr/wombat.h"
 
-void moveservo(int port, int position){
+void moveservo(int port, int position) {
     enable_servos();
     set_servo_position(port, position);
-    msleep(1);
+    msleep(1000);
 }
 
-void setupsensordata(){
+void setupsensordata() {
     set_create_distance(0);
     set_create_total_angle(0);
 }
 
 
-void printsensordata(){
-    printf("%d,%d,%s",get_create_rwdrop(),0,"right wheel drop \n");
-    printf("%d,%d,%s",get_create_rcliff(),0,"right cliff \n");
-    printf("%d,%d,%s",get_create_rlightbump(),0,"right light bumper \n");
-    printf("%d,%d,%s",get_create_rbump(),0,"right bumper \n");
-    printf("%d,%d,%s",get_create_rflightbump(),0,"right light front bumper \n");
-    printf("%d,%d,%s",get_create_rfcliff(),0,"right cliff \n");
-    printf("%d,%d,%s",get_create_rclightbump(),0,"right cliff light bumper \n");
-    printf("%d,%d,%s",get_create_cwdrop(),0,"front drop wheel \n");
-    printf("%d,%d,%s",get_create_lclightbump(),0,"left cliff light bumper \n");
-    printf("%d,%d,%s",get_create_lfcliff(),0,"left front cliff \n");
-    printf("%d,%d,%s",get_create_lflightbump(),0,"left light bumper \n");
-    printf("%d,%d,%s",get_create_lbump(),0,"left bumper \n");
-    printf("%d,%d,%s",get_create_llightbump(),0,"left light bumper \n");
-    printf("%d,%d,%s",get_create_lcliff(),0,"left cliff \n");
-    printf("%d,%d,%s",get_create_lwdrop(),0,"left wheel drop \n");
-    printf("%d,%d,%s",get_create_distance(),0,"total distance\n");
-    printf("%d,%d,%s",get_create_total_angle(),0,"total angle \n");
+void printsensordata() {
+    printf("%d,%d,%s", get_create_rwdrop(), 0, "right wheel drop \n");
+    printf("%d,%d,%s", get_create_rcliff(), 0, "right cliff \n");
+    printf("%d,%d,%s", get_create_rlightbump(), 0, "right light bumper \n");
+    printf("%d,%d,%s", get_create_rbump(), 0, "right bumper \n");
+    printf("%d,%d,%s", get_create_rflightbump(), 0, "right light front bumper \n");
+    printf("%d,%d,%s", get_create_rfcliff(), 0, "right cliff \n");
+    printf("%d,%d,%s", get_create_rclightbump(), 0, "right cliff light bumper \n");
+    printf("%d,%d,%s", get_create_cwdrop(), 0, "front drop wheel \n");
+    printf("%d,%d,%s", get_create_lclightbump(), 0, "left cliff light bumper \n");
+    printf("%d,%d,%s", get_create_lfcliff(), 0, "left front cliff \n");
+    printf("%d,%d,%s", get_create_lflightbump(), 0, "left light bumper \n");
+    printf("%d,%d,%s", get_create_lbump(), 0, "left bumper \n");
+    printf("%d,%d,%s", get_create_llightbump(), 0, "left light bumper \n");
+    printf("%d,%d,%s", get_create_lcliff(), 0, "left cliff \n");
+    printf("%d,%d,%s", get_create_lwdrop(), 0, "left wheel drop \n");
+    printf("%d,%d,%s", get_create_distance(), 0, "total distance\n");
+    printf("%d,%d,%s", get_create_total_angle(), 0, "total angle \n");
 }
 
-bool setupcamera(){
-    if(camera_open() == 0){
+bool setupcamera() {
+    if (camera_open() == 0) {
         return false;
     }
-    if(camera_update() == 0)
-    {
+    if (camera_update() == 0) {
         return false;
     }
     return true;
 }
 
-void setuprobot(bool debug, bool camera){
+void setuprobot(bool debug, bool camera) {
     create_connect();
     setupsensordata();
-    if(debug == true){
+    if (debug == true) {
         printsensordata();
     }
-    if(camera){
-        if(setupcamera()){
-            printf("%s","Camera initialized");
-        }
-        else{
-            printf("%s","Camera Error");
+    if (camera) {
+        if (setupcamera()) {
+            printf("%s", "Camera initialized");
+        } else {
+            printf("%s", "Camera Error");
         }
     }
     enable_servos();
 }
 
-void shutdownrobot(bool camera){
-    if(camera){
+void shutdownrobot(bool camera) {
+    if (camera) {
         camera_close();
     }
     disable_servos();
@@ -71,103 +69,116 @@ void shutdownrobot(bool camera){
     create_disconnect();
 }
 
-void drivestraight(int speed, bool direction, int distance, bool watchfortouch){ //false backwards true forwards distance in mm
-    if(direction == true){
+void drivestraight(int speed, bool direction, int distance,
+                   bool watchfortouch) { //false backwards true forwards distance in mm
+    if (direction == true) {
         set_create_distance(0);
-        while(get_create_distance() <= (distance)){
-            create_drive_direct(speed,speed);
-            if(watchfortouch){
-                if(get_create_lbump() || get_create_rbump() == true){
+        while (get_create_distance() <= (distance)) {
+            create_drive_direct(speed, speed);
+            if (watchfortouch) {
+                if (get_create_lbump() || get_create_rbump() == true) {
                     set_create_distance(distance);
                 }
             }
         }
-    }
-    else{
+    } else {
         set_create_distance(0);
-        while((-1*get_create_distance()) <= (distance)){
-            create_drive_direct(speed*-1,speed*-1);
+        while ((-1 * get_create_distance()) <= (distance)) {
+            create_drive_direct(speed * -1, speed * -1);
         }
     }
 }
-void turn(int speed, int deg){
+
+void turn(int speed, int deg) {
     // deg - nach rechts + nach links
     set_create_distance(0);
     set_create_total_angle(0);
-    if(deg>0){
-     while(get_create_total_angle() <= deg){
-         create_drive_direct(speed * -1,speed);
-     }
-    }
-    else{
-            while(get_create_total_angle() >= deg){
-                create_drive_direct(speed,speed * - 1);
-            }
+    if (deg > 0) {
+        while (get_create_total_angle() <= deg) {
+            create_drive_direct(speed * -1, speed);
         }
+    } else {
+        while (get_create_total_angle() >= deg) {
+            create_drive_direct(speed, speed * -1);
+        }
+    }
 }
 
-void wasd(){
+void wasd() { // function to control robot via webinterface
     bool exit;
     exit = true;
     int speed = 100;
-    while(exit){
+    while (exit) {
         char input;
-        printf("%s","Please provide input:\n");
-        scanf("%c",&input);
-        if(input == 'w'){
-            drivestraight(speed,true,25,true);
+        printf("%s", "Please provide input:\n");
+        scanf("%c", &input);
+        if (input == 'w') {
+            drivestraight(speed, true, 25, true);
         }
-        if(input == 's'){
-            drivestraight(speed,false,25,true);
+        if (input == 's') {
+            drivestraight(speed, false, 25, true);
         }
-        if(input == 'd'){
-            turn(speed/2,-45);
+        if (input == 'd') {
+            turn(speed / 2, -45);
         }
-        if(input == 'a'){
-            turn(speed/2,45);
+        if (input == 'a') {
+            turn(speed / 2, 45);
         }
-        if(input == 'x'){
+        if (input == 'x') {
             exit = false;
         }
-        if(input == 'm'){
-            printf("%s","Please provide speed:\n");
-            scanf("%d",&speed);
+        if (input == 'm') {
+            printf("%s", "Please provide speed:\n");
+            scanf("%d", &speed);
         }
     }
 }
 
-void grapscher(int startposition, int endposition, int port){ // opens and closes claw
-    if(get_servo_position(port) < startposition){
-        moveservo(port,startposition);
-    }
-    moveservo(port,endposition);
+void movey(int servoport, int coord) {
+    moveservo(servoport, coord);
 }
 
-void movearm(int position, bool grabobject,int rotation, int lowerservoid, int grabservoid, int speed){
-    int idlegrabposition = 500; // placeholder
-    if(rotation != 0){ // turn
-        turn(speed, rotation);
-    }
-    moveservo(lowerservoid,position);
-    // claw
-    if(grabobject == true){
-        grapscher(idlegrabposition,550,grabservoid); //placeholder value
+void openclaw(int servoport) {
+    moveservo(servoport, 2047);
+}
+
+
+void closeclaw(int servoport) {
+    moveservo(servoport, 0);
+}
+
+void movex(int servoport, int degrees) { // first axis
+    if (degrees < 0) {
+        moveservo(servoport, degrees * -2);
+    } else {
+        moveservo(servoport, degrees * 2);
     }
 }
 
-int getObject(int channel){
+void levelclaw(int servoportx, int servoporty, int servoportz) {
+    get_servo_position(servoportx);
+    get_servo_position(servoporty);
+    get_servo_position(servoportz);
+}
+
+int getObject(int channel) {
     camera_update();
-    if(get_object_count(channel)){
-        get_object_center_x(channel,0); // 0 ist des größte
-        get_object_center_y(channel,0);
+    if (get_object_count(channel)) {
+        get_object_center_x(channel, 0); // 0 ist des größte
+        get_object_center_y(channel, 0);
 
     }
+    return 0;
 }
-int main()
-{
-    setuprobot(false,false);
+
+void movearm(){
+    // placeholder
+}
+
+int main() {
+    setuprobot(false, false);
     wasd();
-    turn(100,90);
+    turn(100, 90);
     shutdownrobot(false);
     return 0;
 }
