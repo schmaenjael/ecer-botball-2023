97aad6856ec77cdc55f33b17146f706f08a14ac6
Viereckig <49343060+Viereckig@users.noreply.github.com>
2023-03-17 16:22:40 +0100

    changed requested things; made it to the second stair
    changed all things manni requested,
    added comments to the whole code,
    programmed that the ring can go to the second stair

--- src/roomba_robot/tasks/stair_task/stair_task.c
+++ src/roomba_robot/tasks/stair_task/stair_task.c
@@ -7,10 +7,10 @@
 
 void calibration(){
     //normal Positions
-    int servoArm = 1900;
-    int servoTurnArm = 1500;
-    int servoHand = 100;
-    int motorArm = 12000;
+    int servo_arm = 1900;
+    int servo_turn_arm = 1500;
+    int servo_hand = 100;
+    int motor_arm = 12000;
     
     
     while(!digital(0)){
@@ -21,18 +21,18 @@ void calibration(){
     
 
     msleep(5);
-    set_servo_position(0,servoArm);
+    set_servo_position(0,servo_arm);
     msleep(5);
-    set_servo_position(1,servoTurnArm);
+    set_servo_position(1,servo_turn_arm);
     msleep(5);
-    set_servo_position(2,servoHand);
+    set_servo_position(2,servo_hand);
     msleep(500);
     
     
     
     
     
-    while(get_motor_position_counter(0)<=motorArm){
+    while(get_motor_position_counter(0)<=motor_arm){
         motor(0, 100);
     }
     motor(0, 0);
@@ -82,68 +82,84 @@ int main()
     enable_servos();
     create_connect();
     
-    printf("left: %i\nright: %i\n", get_create_lfcliff_amt(), get_create_rfcliff_amt());
-    
     calibration();
     msleep(1);
     
     set_servo_position(0,1500);
     
-    drive_left_of_line();							//driving on black line until hitting wall to find right position
+    drive_left_of_line();				//driving on black line until hitting wall to find right position
     
-    create_drive_direct(-500,-500);					//-------------------------------------------------------------------
+    create_drive_direct(-500,-500);		//-------------------------------------------------------------------
     msleep(1500);
-    create_drive_direct(100,-100);    				//driving back and turning in direction of ring
+    create_drive_direct(100,-100);    	//driving back and turning in direction of ring
     msleep(820);
     create_drive_direct(0,0);
-    clear_motor_position_counter(0);				//-------------------------------------------------------------------
+    clear_motor_position_counter(0);	//-------------------------------------------------------------------
     
-    grappling_position();							//arm goes in right position to pefectly grab the ring
+    grappling_position();				//arm goes in right position to pefectly grab the ring
     
-    create_drive_direct(200,200);					//
-    msleep(2100);									//driving to the ring
-    create_drive_direct(0,0);						//
+    create_drive_direct(200,200);		//
+    msleep(2100);						//driving to the ring
+    create_drive_direct(0,0);			//
     
-    set_servo_position(2,100);						//grab ring
+    set_servo_position(2,100);			//grab ring
     msleep(200);
     
-    create_drive_direct(-100,-100);
-  	msleep(700);
-    create_drive_direct(0,0);
-    set_servo_position(1,0);
-    msleep(100);
+    create_drive_direct(-100,-100);		//
+  	msleep(700);						//driving back, that arm would not hit the vertikal stair
+    create_drive_direct(0,0);			//
     
+    set_servo_position(1,0);			//tilt ring to the left that he can get moved
+    msleep(100);						//to the left on the first stair
     
-    while(get_motor_position_counter(0)>=3000){
+    while(get_motor_position_counter(0)>=3000){		//-----------------------------------------------
         motor(0, -100);
-        msleep(1);
+        msleep(1);									//move arm up
     }
-    motor(0,0);
+    motor(0,0);										//-----------------------------------------------
     
-    create_drive_direct(-100,-100);
-    msleep(300);
-    create_drive_direct(0,0);
+    create_drive_direct(-100,-100);					//
+    msleep(300);									//driving back, that arm would not hit the horizontal stair
+    create_drive_direct(0,0);						//
     
-    while(get_motor_position_counter(0)>=1000){
+    while(get_motor_position_counter(0)>=1000){		//-----------------------------------------------
+        motor(0, -100);							
+        msleep(1);									//move arm up to the height of the first stair
+    }
+    motor(0,0);										//-----------------------------------------------
+    
+    
+    create_drive_direct(-100,100);		//-----------------------------------------------
+  	msleep(300);
+    create_drive_direct(-100,-100);		//drive to the left to get in position for the next stair 
+    msleep(200);
+    create_drive_direct(0,0);			//-----------------------------------------------
+    
+    set_servo_position(1,600);			//turn ring vertikal to go up the horizontal stair
+  	msleep(300);
+    
+    while(get_motor_position_counter(0)>=-1500){		//-----------------------------------------------
         motor(0, -100);
-        msleep(1);
+        msleep(1);										//move arm up
     }
-    motor(0,0);
+    motor(0,0);											//-----------------------------------------------
     
+    set_servo_position(1,0);			//tilt ring to the left that he can get moved to the left on the second stair
+														
     
-    create_drive_direct(-100,100);
-  	msleep(300);
-    create_drive_direct(-100,-100);
-    msleep(300);
-    create_drive_direct(0,0);
-    set_servo_position(1,600);
-  	msleep(300);
+    create_drive_direct(-100,-100);		//
+    msleep(200);						//driving back, that arm would not hit the horizontal stair
+    create_drive_direct(0,0);			//
     
-    while(get_motor_position_counter(0)>=0){
-        motor(0, -2000);
-        msleep(1);
+    while(get_motor_position_counter(0)>=-3500){		//-----------------------------------------------
+        motor(0, -100);
+        msleep(1);										//move arm up to the height of the second stair
     }
+    motor(0,0);											//-----------------------------------------------
     
+    create_drive_direct(-100,50);						//
+    msleep(400);										//drive to the left to get in position for the next stair
+    create_drive_direct(0,0);							//
     
     
     //turn_arm_back();
